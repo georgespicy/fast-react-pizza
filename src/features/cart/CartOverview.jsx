7e3dfce224603b639bd21e8bc62c99123d1fa60c
georgespicy <kolawolegeorge28743@gmail.com>
2024-04-23 18:10:34 +0100

    handling state management using redux and done with the basic functionality

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,11 +1,18 @@
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { getTotalCartPrice, getTotalCartQuantity } from "./cartSlice";
 
 function CartOverview() {
+  const totalCartQuatity = useSelector(getTotalCartQuantity);
+  const totalCartPrice = useSelector(getTotalCartPrice);
+
+  if (!totalCartQuatity) return null;
+
   return (
     <div className="md:text-base- flex items-center justify-between bg-stone-800 p-4 px-4 py-4 uppercase text-stone-200 sm:px-6 md:text-base">
       <p className="sm:space-6 space-x-4 font-semibold text-stone-300">
-        <span>23 pizzas</span>
-        <span>$23.45</span>
+        <span>{totalCartQuatity} pizzas</span>
+        <span>${totalCartPrice}</span>
       </p>
       <Link to="/cart">Open cart &rarr;</Link>
     </div>
